6a295735395d80beb6a24a8cc7845ade0928e331
agente138 <mike.hernando.140902@gmail.com>
2024-06-11 19:41:03 -0500

    Add events Listeners and buttons

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
-const container = document.querySelector(".container");
+const createGrid = (n) => {
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < n; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row-grid")
 
-for (let i = 0; i < 16; i++) {
-    let div = document.createElement("div");
-    for (let j = 0; j < 16; j++) {
-        let subDiv = document.createElement("div");
-        div.appendChild(subDiv);
+        for (let j = 0; j < n; j++) { 
+            let gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");           
+            gridItem.addEventListener("mouseout", e => {
+                e.target.style.backgroundColor = "black";               
+            });
+            row.append(gridItem);
+        }
+        fragment.append(row);    
     }
-    container.appendChild(div);
+
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.appendChild(fragment);
+    return grid;
 }
+const container = document.querySelector(".container");
+var n = 16;
+const grid = createGrid(n)
+container.append(grid);
+
+
+newGridBtn = document.querySelector(".new-grid-btn");
+resetBtn = document.querySelector(".reset-btn");
+
+newGridBtn.addEventListener("click", () => {
+    const child = document.querySelector(".grid");
+    console.log(child);
+    container.removeChild(child);
+    do {
+        n = prompt("Grid size (n x n):");
+    } while (n < 1 || n > 100);    
+    const grid = createGrid(n);
+    container.append(grid);
+});
+
+resetBtn.addEventListener("click", () => {
+    const child = document.querySelector(".grid");
+    console.log(child);
+    container.removeChild(child);   
+    const grid = createGrid(n);
+    container.append(grid);
+});
